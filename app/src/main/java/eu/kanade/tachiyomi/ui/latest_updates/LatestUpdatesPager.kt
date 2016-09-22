181af78447c0ea53219422df7bfec0bc46c93ef7
Roman Martinez <rmnmrtnz23@gmail.com>
2016-09-21 21:46:37 -0500

    LatestUpdates now inherites from Catalogue Thanks for your help inorichi!

--- app/src/main/java/eu/kanade/tachiyomi/ui/latest_updates/LatestUpdatesPager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/latest_updates/LatestUpdatesPager.kt
@@ -2,20 +2,12 @@ package eu.kanade.tachiyomi.ui.latest_updates
 
 import eu.kanade.tachiyomi.data.source.model.MangasPage
 import eu.kanade.tachiyomi.data.source.online.OnlineSource
+import eu.kanade.tachiyomi.ui.catalogue.Pager
 import rx.Observable
-import rx.subjects.PublishSubject
 
-class LatestUpdatesPager(val source: OnlineSource) {
+class LatestUpdatesPager(val source: OnlineSource): Pager() {
 
-    private var lastPage: MangasPage? = null
-
-    private val results = PublishSubject.create<MangasPage>()
-
-    fun results(): Observable<MangasPage> {
-        return results.asObservable()
-    }
-
-    fun requestNext(transformer: (Observable<MangasPage>) -> Observable<MangasPage>): Observable<MangasPage> {
+    override fun requestNext(transformer: (Observable<MangasPage>) -> Observable<MangasPage>): Observable<MangasPage> {
         val lastPage = lastPage
 
         val page = if (lastPage == null)
@@ -23,13 +15,11 @@ class LatestUpdatesPager(val source: OnlineSource) {
         else
             MangasPage(lastPage.page + 1).apply { url = lastPage.nextPageUrl!! }
 
-        return transformer(source.fetchLatestUpdates(page))
+        val observable = source.fetchLatestUpdates(page)
+
+        return transformer(observable)
                 .doOnNext { results.onNext(it) }
                 .doOnNext { this@LatestUpdatesPager.lastPage = it }
-    }
-
-    fun hasNextPage(): Boolean {
-        return lastPage == null || lastPage?.nextPageUrl != null
     }
 
 }
